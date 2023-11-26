b99922e2bd835c066541d288ac3f8a2667be11a7
VAS-IDE <vas.ide2022@gmail.com>
2023-11-26 13:18:32 +0300

    QEA

---
+++ otus/02 - API/1_pytest_addoption/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        action="store",
+        default="https://ya.ru",
+        help="This is request url"
+    )
+
+
+@pytest.fixture
+def url_param(request):
+    print(request.config.getoption("--url"))
+    return request.config.getoption("--url")
---
+++ otus/02 - API/1_pytest_addoption/test_demo_param.py
@@ -0,0 +1,8 @@
+def test_answer(url_param):
+    if url_param == "https://ya.ru":
+    # if url_param == "ya.ru":
+        print("YAAAAANDEX")
+    elif url_param == "google.com":
+        print("GOOOGLE!")
+    else:
+        print("DuckDuckGOOOOO")
