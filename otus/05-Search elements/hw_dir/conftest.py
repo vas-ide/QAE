c3e5fb49aa847f410fc2c1b4d0e4907752fc5a06
VAS <vas.ide2022@gmail.com>
2023-12-31 18:19:50 +0300

    QA e

--- otus/05-Search elements/hw_dir/conftest.py
+++ otus/05-Search elements/hw_dir/conftest.py
@@ -5,8 +5,9 @@ from selenium import webdriver
 from selenium.webdriver import ChromeOptions, FirefoxOptions, EdgeOptions
 
 
-# def pytest_addoption(parser):
-    # parser.addoption("--browser", "-B", action="store", default="chrome", help="base browser")
+def pytest_addoption(parser):
+
+    parser.addoption("--browser", "-B", action="store", default="chrome", help="base browser")
     # parser.addoption("--url", "-U", action="store", default="https://google.com", help="base url")
     # parser.addoption("--url", "-U", action="store", default="https://localhost/", help="base url")
     # parser.addoption("--url", "-U", action="store", default="https://localhost/opencart", help="base url")
@@ -14,10 +15,8 @@ from selenium.webdriver import ChromeOptions, FirefoxOptions, EdgeOptions
 
 
 @pytest.fixture
-def browser(request, browser_param=None):
-    if browser_param is None:
-        browser_param = request.config.getoption("--browser")
-
+def browser(request):
+    browser_param = request.config.getoption("--browser")
     if browser_param == "chrome":
         options = ChromeOptions()
         # options.add_argument("--headless")
@@ -37,8 +36,36 @@ def browser(request, browser_param=None):
     else:
         raise Exception(f"{request.param} is not supported!")
 
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(3)
     request.addfinalizer(driver.close)
-    driver.get(request.config.getoption("--url"))
+    # driver.get(request.config.getoption("--url"))
+
+    return driver
+
+
+@pytest.fixture(params=["chrome", "edge", "firefox"])
+def parametrize_browser(request):
+    browser_param = request.param
+    if browser_param == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--start-fullscreen")
+        driver = webdriver.Chrome(options=options)
+    elif browser_param == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--start-fullscreen")
+        driver = webdriver.Firefox(options=options)
+    elif browser_param == "edge":
+        options = EdgeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--start-fullscreen")
+        driver = webdriver.Edge(options=options)
+    else:
+        raise Exception(f"{request.param} is not supported!")
+
+    driver.implicitly_wait(3)
+    request.addfinalizer(driver.quit)
+    # driver.get(request.config.getoption("--url"))
 
     return driver
--- otus/05-Search elements/hw_dir/test_hw.py
+++ otus/05-Search elements/hw_dir/test_hw.py
@@ -1,5 +1,7 @@
 import time
 import os
+
+import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as Service_edge
 from selenium.webdriver.chrome.service import Service as Service_chrome
@@ -20,23 +22,76 @@ from locators import MainLogo, CartIPhone, CartMacBook
 # options_chrome = ChromeOptions()
 # options_chrome.add_argument("--start-fullscreen")
 # driver = webdriver.Chrome(service=ser_chrome)
-# driver = webdriver.Chrome()
+# driver = webdriver.Chrome(options=options_chrome)
 # ser_firefox = Service_firefox(executable_path="/home/vas/Documents/Python/QAE/otus/driver/geckodriver-v0.33.0-linux64/geckodriver")
 # driver = webdriver.Firefox(service=ser_firefox)
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
+# #
+# try:
+#     # driver.maximize_window()
+#     driver.get("http://localhost/opencart")
+#     main_macbook = driver.find_element(By.PARTIAL_LINK_TEXT, CartMacBook.cart_partial_link_text).click()
+#     time.sleep(1)
+#     logo = driver.find_element(By.XPATH, MainLogo.logo_xpath).click()
+#     time.sleep(1)
+#     main_iphone = driver.find_element(By.PARTIAL_LINK_TEXT, CartIPhone.cart_partial_link_text).click()
+#     time.sleep(1)
+# except Exception as ex:
+#     print(ex)
+# finally:
+#     driver.close()
+#     driver.quit()
 
-try:
-    driver.maximize_window()
-    driver.get("http://localhost/opencart")
-    main_macbook = driver.find_element(By.PARTIAL_LINK_TEXT, CartMacBook.cart_partial_link_text).click()
-    time.sleep(1)
-    logo = driver.find_element(By.XPATH, MainLogo.logo_xpath).click()
-    time.sleep(1)
-    main_iphone = driver.find_element(By.PARTIAL_LINK_TEXT, CartIPhone.cart_partial_link_text).click()
-    time.sleep(1)
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
 
+class TestOpenCart():
+    base_url = "http://localhost/opencart"
+
+    #
+    # @pytest.fixture(params=["chrome", "edge", "firefox"])
+    # def parametrize_browser(request):
+    #     browser_param = request.param
+    #     if browser_param == "chrome":
+    # @pytest.mark.browser_param("edge")
+    # @pytest.param(browser_param = "edge")
+
+    def test_simple_browser(self, browser):
+        browser_param = "edge"
+        # browser_param = "edge"
+        driver = browser
+        try:
+            driver.get(self.base_url)
+            main_macbook = driver.find_element(By.PARTIAL_LINK_TEXT, CartMacBook.cart_partial_link_text).click()
+            time.sleep(1)
+            logo = driver.find_element(By.XPATH, MainLogo.logo_xpath).click()
+            time.sleep(1)
+            main_iphone = driver.find_element(By.PARTIAL_LINK_TEXT, CartIPhone.cart_partial_link_text).click()
+            time.sleep(1)
+        except Exception as ex:
+            print(ex)
+
+    # def test_simple_browser(self, browser):
+    #     # driver = browser(browser_param="edge")
+    #     driver = browser
+    #     try:
+    #         driver.get(self.base_url)
+    #         main_macbook = driver.find_element(By.PARTIAL_LINK_TEXT, CartMacBook.cart_partial_link_text).click()
+    #         time.sleep(1)
+    #         logo = driver.find_element(By.XPATH, MainLogo.logo_xpath).click()
+    #         time.sleep(1)
+    #         main_iphone = driver.find_element(By.PARTIAL_LINK_TEXT, CartIPhone.cart_partial_link_text).click()
+    #         time.sleep(1)
+    #     except Exception as ex:
+    #         print(ex)
+
+    # def test_all_browsers(self, parametrize_browser):
+    #     driver = parametrize_browser
+    #     try:
+    #         driver.get(self.base_url)
+    #         main_macbook = driver.find_element(By.PARTIAL_LINK_TEXT, CartMacBook.cart_partial_link_text).click()
+    #         time.sleep(1)
+    #         logo = driver.find_element(By.XPATH, MainLogo.logo_xpath).click()
+    #         time.sleep(1)
+    #         main_iphone = driver.find_element(By.PARTIAL_LINK_TEXT, CartIPhone.cart_partial_link_text).click()
+    #         time.sleep(1)
+    #     except Exception as ex:
+    #         print(ex)
